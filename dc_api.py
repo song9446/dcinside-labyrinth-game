ec2111b3c75ea68e4f03cd976deee840456f1901
Eunchul <song9446@unist.ac.kr>
2018-04-07 05:33:58 +0900

    floor 3

---
+++ dc_api.py
@@ -0,0 +1,343 @@
+#!/usr/bin/python3
+
+import sys
+import requests
+import re
+from datetime import timedelta, date, datetime
+import json
+import time
+import urllib
+import urllib.parse
+#from collections import OrderedDict
+#import logging
+#logging.basicConfig(level=logging.DEBUG)
+
+from pprint import pprint
+
+GET_HEADERS = {
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36",
+    "Upgrade-Insecure-Requests": "1",
+    "Host": "m.dcinside.com",
+    "Connection": "keep-alive",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    }
+
+POST_HEADERS = {
+    "Accept": "*/*",
+    "Accept-Encoding": "gzip, deflate",
+    "Accept-Language": "ko-KR,ko;q=0.9,en-US;q=0.8,en;q=0.7",
+    "Connection": "keep-alive",
+    "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
+    "Host": "m.dcinside.com",
+    "Origin": "http://m.dcinside.com",
+    "Referer": "http://m.dcinside.com/write.php?id=alphago&mode=write",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/65.0.3325.181 Mobile Safari/537.36",
+    "X-Requested-With": "XMLHttpRequest",
+    }
+
+def iterableBoard(board, is_miner=False, num=-1, start_page=1, sess=None):
+    # create session
+    if sess is None:
+        sess = requests.session()
+    url = "http://m.dcinside.com/list.php"
+    params = { "id": board, "page": str(start_page) }
+    i = 0
+    last_doc_no = 0
+    doc_in_page = 0
+    page = start_page
+    header = GET_HEADERS.copy()
+    header["Referer"] = url
+    while num != 0:
+        params["page"] = str(page)
+        res = sess.get(url, headers=header, params=params, timeout=10)
+        t, start = raw_parse(res.text, '"list_best">', "<", i)
+        t, end = raw_parse(res.text, "</ul", ">", start)
+        i = start
+        while num != 0 and i < end and i >= start:
+            doc_no, i = raw_parse(res.text, 'no=', '&', i)
+            if i >= end or i == 0:
+                break
+            doc_no = int(doc_no)
+            if last_doc_no != 0 and doc_no >= last_doc_no:
+                continue
+            last_doc_no = doc_no
+            t, i = raw_parse(res.text, 'ico_pic ', '"', i)
+            has_image = (t == "ico_p_y")
+            title, i = raw_parse(res.text, 'txt">', '<', i)
+            t, i = raw_parse(res.text, 'txt_num">', "<", i)
+            comments = int(t[1:-1]) if len(t)>0 else 0
+            name, i = raw_parse(res.text, 'name">', "<", i)
+            t, i = raw_parse(res.text, 'class="', '"', i)
+            ip = None
+            if t == "userip":
+                ip, i = raw_parse(res.text, '>', '<', i) 
+            date, i = raw_parse(res.text, "<span>", "<", i)
+            t, i = raw_parse(res.text, '조회', "<", i)
+            views, i = raw_parse(res.text, '>', '<', i)
+            t, i = raw_parse(res.text, '추천', "<", i)
+            votes, i = raw_parse(res.text, '>', '<', i)
+            yield {
+                "doc_no": doc_no, "title": title, "name": name, "ip": ip, "date": date, "views": int(views), "votes": int(votes), "comments": int(comments)
+                }
+            num -= 1
+        page += 1
+        
+def iterableComments(board, is_miner, doc_no, num=-1, sess=None):
+    if sess is None:
+        sess = requests.session()
+    referer = "http://m.dcinside.com/view.php?id=%s&no=%s" % (board, doc_no)
+    url = "http://m.dcinside.com/%s/comment_more_new.php" % ("m" if is_miner else "")
+    page = 1
+    params = {"id": board, "no": str(doc_no), "com_page": str(page)}
+    headers = GET_HEADERS.copy()
+    headers["Referer"] = referer
+    num_comments, i, count = 999999999,0,0
+    
+    while num != 0:
+        params["com_page"] = str(page)
+        res = sess.get(url, headers=headers, params=params, timeout=10)
+        t, i = raw_parse(res.text, 'txt_total">(', ')', i)
+        if i==0: break
+        num_comments = min(num_comments, int(t))
+        i = -1
+        while num != 0:
+            date, i = rraw_parse(res.text, '"date">', '<', i)
+            if i==0: break
+            contents, i = rraw_parse(res.text, '"txt">', '="info">', i)
+            ip, i = rraw_parse(res.text, '"ip">', '<', i)
+            name, i = rraw_parse(res.text, '>[', ']<', i)
+            name = name.replace('<span class="nick_comm flow"></span>', "")
+            name = name.replace('<span class="nick_comm fixed"></span>', "")
+            name = name.replace('<span class="nick_mnr_comm ic_gc_df"></span>', "")
+            yield {
+                "name": name.strip(), "ip": ip.strip(), "contents": contents[:-66].strip(), "date": date.strip()
+                }
+            num -= 1
+            count += 1
+        if count >= num_comments:
+            break
+        else:
+            page += 1
+
+def writeDoc(board, is_miner, name, password, title, contents, sess=None):
+    # create session
+    if sess is None:
+        sess = requests.Session()
+    url = "http://m.dcinside.com/write.php?id=%s&mode=write" % board
+    res = sess.get(url, headers=GET_HEADERS)
+    # get secret input
+    data = extractKeys(res.text, 'g_write.php"')
+    if name: data['name'] = name
+    if password: data['password'] = password
+    data['subject'] = title
+    data['memo'] = contents
+    # get new block key
+    headers = POST_HEADERS.copy()
+    headers["Referer"] = url
+    url = "http://m.dcinside.com/_option_write.php"
+    
+    verify_data = {
+        "id": data["id"],
+        "w_subject": title,
+        "w_memo": contents,
+        "w_filter": "",
+        "mode": "write_verify",
+    }
+    new_block_key = sess.post(url, data=verify_data, headers=headers).json()
+    if new_block_key["msg"] != "5":
+        print("Error wile write doc(block_key)")
+        print(result)
+        exit(1)
+    data["Block_key"] = new_block_key["data"]
+    url = "http://upload.dcinside.com/g_write.php"
+    result = sess.post(url, data=urllib.parse.urlencode(data, True), headers=headers).text
+    doc_no, i = raw_parse(result, "no=", '"')
+    return doc_no
+
+def removeDoc(board, is_miner, doc_no, password, sess=None):
+    # create session
+    if sess is None:
+        sess = requests.Session()
+    headers = POST_HEADERS.copy()
+    data = {"no": doc_no, "id": board, "page": "", "mode": "board_del"}
+    if password:
+        url = "http://m.dcinside.com/_access_token.php"
+        headers["Referer"] = "http://m.dcinside.com/password.php?id=%s&no=%s&mode=board_del2&flag=" % (board, doc_no)
+        result = sess.post(url, data={"token_verify": "nonuser_del"}, headers=headers).json()
+        if result["msg"] != "5":
+            print("Error wile write doc(block_key)")
+            print(result)
+            exit(1)
+        data["mode"] = "board_del2"
+        data["write_pw"] = password
+        data["con_key"] = result["data"]
+    else:
+        url = "http://m.dcinside.com/view.php?id=%s&no=%s" % (board, doc_no)
+        res = sess.get(url, headers=GET_HEADERS)
+        user_no = raw_parse(res.text, '"user_no" value="', '"')[0]
+        headers["Referer"] = url
+        data["mode"] = "board_del"
+        data["user_no"] = user_no
+    url = "http://m.dcinside.com/_option_write.php"
+    result = sess.post(url, data=data, headers=headers).json()
+    if (type(result)==int and result != 1) or (type(result)==dict and result["msg"] != "1"):
+        print("Error while remove doc: ", result)
+        exit(1)
+    return sess
+
+
+def writeComment(board, is_miner, doc_no, name, password, contents, sess=None):
+    # create session
+    if sess is None:
+        sess = requests.Session()
+    url = "http://m.dcinside.com/view.php?id=%s&no=%s" % (board, doc_no)
+    res = sess.get(url, headers=GET_HEADERS, timeout=10)
+    data = extractKeys(res.text, '"comment_write"')
+    if name: data["comment_nick"] = name
+    if password: data["comment_pw"] = password
+    data["comment_memo"] = contents
+    headers = POST_HEADERS.copy()
+    headers["Referer"] = url
+    url = "http://m.dcinside.com/_access_token.php"
+    block_key = sess.post(url, headers=headers, data={"token_verify": "com_submit"}, timeout=10).json()
+    if block_key["msg"] != "5":
+        print("Error wile write comment(block key)")
+        exit(1)
+    url = "http://m.dcinside.com/_option_write.php"
+    data["con_key"] = block_key["data"]
+    result = sess.post(url, headers=headers, data=data, timeout=10)
+    result = result.json()
+    if result["msg"] != "1":
+        print("Error wile write comment")
+        exit(1)
+    return doc_no
+    
+
+def login(userid, password, sess=None):
+    if sess is None:
+        sess = requests.Session()
+    url = "http://m.dcinside.com/login.php?r_url=m.dcinside.com%2Findex.php"
+    headers = GET_HEADERS.copy()
+    headers["Referer"] = "http://m.dcinside.com/index.php"
+    res = sess.get(url, headers=headers, timeout=10)
+    data = extractKeys(res.text, '"login_process')
+    print(res.text)
+    headers = POST_HEADERS.copy()
+    headers["Referer"] = url
+    url = "http://m.dcinside.com/_access_token.php"
+    res = sess.post(url, headers=headers, data={"token_verify": "login", "con_key": data["con_key"]}, timeout=10)
+    data["con_key"] = res.json()["data"]
+    url = "https://dcid.dcinside.com/join/mobile_login_ok.php"
+    headers["Host"] = "dcid.dcinside.com"
+    headers["Accept"] = "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8"
+    headers["Accept-Encoding"] = "gzip, deflate, br"
+    headers["Cache-Control"] = "max-age=0"
+    headers["Content-Type"] = "application/x-www-form-urlencoded"
+    del(headers["X-Requested-With"])
+    data["user_id"] = userid
+    data["user_pw"] = password
+    data["id_chk"] = "on"
+    #data["mode"] = ""
+    if "form_ipin" in data: del(data["form_ipin"])
+    res = sess.post(url, headers=headers, data=data, timeout=10)
+    while 0 <= res.text.find("rucode"):
+        print("login fail!")
+        print(res.text)
+        time.sleep(5)
+        res = sess.post(url, headers=headers, data=data, timeout=10)
+    return sess
+    
+def logout(sess):
+    url = "http://m.dcinside.com/logout.php?r_url=m.dcinside.com%2Findex.php"
+    headers = GET_HEADERS.copy()
+    headers["Referer"] = "http://m.dcinside.com/index.php"
+    res = sess.get(url, headers=headers, timeout=10)
+    return sess
+    
+def extractKeys(html, start_form_keyword):
+    p = ""
+    start, end, i = 0, 0, 0
+    #result = []#OrderedDict()
+    result = {}
+    (p, start) = raw_parse(html, start_form_keyword, '', i)
+    (p, end) = raw_parse(html, '</form>', '', start)
+    i = start
+    while True:
+        (p, i) = raw_parse(html, '<input type="hidde', '"', i)
+        if not p or i >= end: break
+        (name, i) = raw_parse(html, 'name="', '"', i)
+        if not name or i >= end: break
+        (value, i_max) = raw_parse(html, '', '>', i)
+        (value, i) = raw_parse(html, 'value="', '"', i)
+        if i_max > i:
+            result[name] = value
+            #result.append((i, name, value))
+        else:
+            i = i_max
+            result[name] = ""
+            #result.append((i, name, ""))
+    i = start
+    while True:
+        (p, i) = raw_parse(html, "<input type='hidde", "'", i)
+        if not p or i >= end: break
+        (name, i) = raw_parse(html, "name='", "'", i)
+        if not name or i >= end: break
+        (value, i_max) = raw_parse(html, '', '>', i)
+        (value, i) = raw_parse(html, "value='", "'", i)
+        if i_max > i:
+            #result.append((i, name, value))
+            result[name] = value
+        else:
+            i = i_max
+            #result.append((i, name, ""))
+            result[name] = ""
+    while True:
+        (p, i) = raw_parse(html, '<input type="hidde', '"', i)
+        if not p or i >= end: break
+        (name, i) = raw_parse(html, 'NAME="', '"', i)
+        if not name or i >= end: break
+        (value, i_max) = raw_parse(html, '', '>', i)
+        (value, i) = raw_parse(html, 'value="', '"', i)
+        if i_max > i:
+            result[name] = value
+            #result.append((i, name, value))
+        else:
+            i = i_max
+            result[name] = ""
+            #result.append((i, name, ""))
+    return result
+    #return [i[1:] for i in sorted(result, key=lambda x: x[0])]
+
+def rraw_parse(text, start, end, offset=0):
+    s = text.rfind(start, 0, offset)
+    if s == -1: return None, 0
+    s += len(start)
+    e = text.find(end, s)
+    if e == -1: return None, 0
+    return text[s:e], s - len(start)
+
+def raw_parse(text, start, end, offset=0):
+    s = text.find(start, offset)
+    if s == -1: return None, 0
+    s += len(start)
+    e = text.find(end, s)
+    if e == -1: return None, 0
+    return text[s:e], e
+
+if __name__ == '__main__':
+    print("login and get session..")
+    #sess = login("sech9446", "song4627")
+    #doc_no = writeDoc("alphago", True, None, None, "알파고님 동물원 언제만드시냐2", "거기2 들어가면 알파고님이 교배시켜주시겠지? ㅎㅎㅎㅎ", sess=sess)
+    #doc_no = writeComment("alphago", True, doc_no, None, None, "거기 들어가면 알파고님이 교배시켜주시겠지? ㅎㅎㅎㅎ", sess=sess)
+    #removeDoc("alphago", True, "276", None, sess)
+    #logout(sess=sess)
+    #removeDoc("alphago", True, "279", "1234")
+    exit(1)
+    #res = writeDoc("얄파고", "1234", "alphago", "알파고님 동물원 언제만드시냐", "거기 들어가면 알파고님이 교배시켜주시겠지? ㅎㅎㅎㅎ")
+    #[print(i) for i in iterableBoard(board="programming", num=100)]
+    #exit(1)
+    #res = writeDoc("얄파고", "1234", "programming", "지잡대 1학년 python수업 조교하는데", "<p>애들 진짜 개멍청함 파이썬을 어려워할줄은 상상도 못했는데. 내가볼때 고딩이랑 대딩사이에 1년정도 강제 휴식기간 있어야함 어차피 1학년때 애들 진짜 공부 절대 안함 밤새 술마시고 꼴아가지고 오는애들도 종종 보임</p>")
+    #res = writeDoc("test", "1234", "programming", False, "test", "test")
+    prininitialt(res)
---
+++ labyrinth.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python3
+import dc_api
+import os
+import re
+import time
+import datetime
+import korean
+
+class SafeDict(dict):
+    def __missing__(self, key):
+        return '{' + str(key) + '}'
+
+SCENE_TEMPLATE_PATH = "scenes/template_bgcolor_fontcolor_floor_description_problem_hint.html"
+SCENES_PATH = "scenes"
+SCENES_COMPONENT_PATH_FORMAT = SCENES_PATH + "/{floor}/{component}.html"
+COMPONENTS = ["description", "problem", "hint", "answer"]
+#NAME="미궁"
+#PASSWORD="1234"
+TITLE_FORMAT_ATTRACT="지하{floor}층에 갇혔다ㅠㅠ 나좀 도와줘"
+TITLE_FORMAT_GENERAL = "지하 {floor}층에 갇혔다ㅠㅠ ({name}이(가) 문열어줌ㅎㅎ)"
+FLOOR_ALIVE_TIME = datetime.timedelta(hours=1)
+ANSWER_COMMENT="{name} 암호를 입력하자 문이 열렸다. 방에 갇혀있던 사람들은 일말의 불안감을 느끼며 천천히 계단을 내려가기 시작했다..."
+
+def contrastFontColor(bgcolor):
+    r = bgcolor // 0x010000
+    g = (bgcolor-r*0x010000) // 0x000100
+    b = bgcolor-r*0x010000-g*0x000100
+    yiq = (r*299 + g*587 + b*114)/1000
+    return 0x000000 if yiq >= 128 else 0xffffff
+
+def hex2rgba(color):
+    r = color // 0x01000000
+    g = (color-r*0x01000000) // 0x00010000
+    b = (color-r*0x01000000-g*0x00010000) // 0x00000100
+    a = color-r*0x01000000-g*0x00010000-b*0x00000100
+    return "rgba(%d, %d, %d, %f)" % (r, g ,b, a/256.)
+def compareAnswer(answer1, answer2):
+    return answer1.upper().replace(' ', '').startswith(answer2.upper().replace(' ', '')) or answer2.upper().replace(' ', '').startswith(answer1.upper().replace(' ', ''))
+
+def createScene(floor):
+    scene_text_template = ""
+    components = {}
+    with open(SCENE_TEMPLATE_PATH, "rt") as scene_file:
+        scene_text_template = re.sub(r'\n\s*', '', scene_file.read())
+    for comp in COMPONENTS:
+        with open(SCENES_COMPONENT_PATH_FORMAT.format(floor=floor, component=comp), "rt") as scene_file:
+            components[comp] = re.sub(r'\n\s*', '', scene_file.read())
+    bgcolor = max(0xddddff66 - (floor-1)*0x11111100, 0x00000066)
+    fontcolor = 0xf3f3f3ff#contrastFontColor(bgcolor)
+    scene = scene_text_template.format(
+        bgcolor="#%08x" % bgcolor,
+        fontcolor="#%08x" % fontcolor,
+        floor=floor,
+        description=components["description"],
+        problem=components["problem"],
+        hint=components["hint"])
+    return scene, components["answer"].upper().replace(' ', '')
+
+def run(board, is_miner, userid, secret):
+    sess = dc_api.login(userid, secret)
+    max_floor = sum(1 for i in os.listdir(SCENES_PATH) if os.path.isdir(os.path.join(SCENES_PATH, i)))
+    answerer = ""
+    for floor in range(1, max_floor+1):
+        try:
+            scene, answer = createScene(floor)
+            title_format = TITLE_FORMAT_GENERAL
+            if floor==1:
+                recent_fun_article = max(dc_api.iterableBoard(board=board, is_miner=is_miner, num=50, sess=sess), key=lambda doc: doc["views"] + doc["votes"]*10)
+                scene = scene.format_map(SafeDict(recent_fun_article=("%s [%d]" % (recent_fun_article["title"], recent_fun_article["comments"])), name=answerer))
+                title_format = TITLE_FORMAT_ATTRACT
+            else:
+                scene = scene.format_map(SafeDict(name=answerer))
+            scene = korean.l10n.proofread(scene)
+            doc_no = dc_api.writeDoc(board=board, is_miner=is_miner, name=None, password=None, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=answerer))), contents=scene, sess=sess)
+            solved = False
+            answerer = ""
+            while not solved:
+                for comment in dc_api.iterableComments(board=board, is_miner=is_miner, doc_no=doc_no, num=20, sess=sess):
+                    if compareAnswer(answer, comment["contents"]):
+                        #dc_api.writeComment(board=board, is_miner=is_miner, doc_no=doc_no, name=None, password=None, contents=ANSWER_COMMENT.format(name=Noun(comment["name"])), sess=sess)
+                        answerer = comment["name"]
+                        print(answerer)
+                        solved = True
+                        floor += 1
+                        dc_api.removeDoc(board=board, is_miner=is_miner, doc_no=doc_no, password=None, sess=sess)
+                        print("floor: %d" % floor)
+                #for doc in dc_api.iterableBoard(board=board, is_miner=is_miner, num=20, sess=sess):
+                #    if compareAnswer(answer, doc["title"]):
+                #        dc_api.writeComment(board=board, is_miner=is_miner, doc_no=doc["doc_no"], name=None, password=None, contents=ANSWER_COMMENT.format(name=Noun(doc["name"])), sess=sess)
+                #        solved = True
+                time.sleep(1)
+        except:
+            floor -= 1
+            continue
+        
+
+if __name__ == '__main__':
+    run("alphago", True, "sech9446", "song4627")
