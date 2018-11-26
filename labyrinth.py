473eb31ebefe74ac40088eed26dadd55f7e3dd4e
Eunchul Song <song9446@unist.ac.kr>
2018-11-27 08:09:40 +0900

    prototype

--- labyrinth.py
+++ labyrinth.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import sys
+sys.path.append("dcinside-python3-api")
 import dc_api
 import os
 import re
@@ -13,18 +15,22 @@ class SafeDict(dict):
     def __missing__(self, key):
         return '{' + str(key) + '}'
 
+COLLAPSED_THRESHOLD = 2*60*60
+COLLAPSED_THRESHOLD = 30
+BREAK_TIME = 5*60
+
 SCENE_TEMPLATE_PATH = "scenes/template_bgcolor_fontcolor_floor_description_problem_hint.html"
 SCENES_PATH = "scenes"
 SCENES_COMPONENT_PATH_FORMAT = SCENES_PATH + "/{floor}/{component}.html"
 COMPONENTS = ["description", "problem", "hint", "answer"]
-BOARD="game_dev"
-IS_MINER=True
-ID="sech9446"
-SECRET="song4627"
+BOARD="alphago"
+ID="bot123"
+SECRET="1q2w3e4r!"
 NAME=None
 PASSWORD=None
-TITLE_FORMAT_ATTRACT="지하{floor}층에 갇혔다.."
-TITLE_FORMAT_GENERAL = "지하{floor}층에 갇혔다..(문연사람: {name})"
+TITLE_FORMAT_ATTRACT="[미궁] 지하{floor}층에 갇혔다.."
+TITLE_FORMAT_GENERAL = "[미궁] 지하{floor}층에 갇혔다..({name}이(가) 문을 열었음)"
+TITLE_FORMAT_COLLAPSED = "[미궁] 시간이 지나 모두들 죽고 말았습니다.."
 FLOOR_ALIVE_TIME = datetime.timedelta(hours=1)
 ANSWER_COMMENT="{name}이(가) 암호를 입력하자 문이 열렸다. 방에 갇혀있던 사람들은 일말의 불안감을 느끼며 천천히 계단을 내려가기 시작했다..."
 
@@ -66,62 +72,84 @@ def createScene(floor):
         hint=components["hint"])
     return scene, components["answer"].upper().replace(' ', '').split("\n")
 
-def run(board, is_miner, userid, secret):
-    sess = dc_api.login(userid, secret)
-    max_floor = sum(1 for i in os.listdir(SCENES_PATH) if os.path.isdir(os.path.join(SCENES_PATH, i)))
+def run(board, userid=None, secret=None):
+    if userid: 
+        sess = dc_api.login(userid, secret)
+    else:
+        sess = dc_api.gen_session()
+    max_floor = sum(1 for i in os.listdir(SCENES_PATH) if os.path.isdir(os.path.join(SCENES_PATH, i)) and i.isdigit() and int(i)>0)
+    print(max_floor)
+    commenters = []
     answerers = []
     last_answerer = ""
-    doc_no = ""
-    last_answers = []
+    doc_id = ""
+    start_time = time.time()
     for floor in range(1, max_floor+1):
         try:
             scene, answers = createScene(floor)
             title_format = TITLE_FORMAT_GENERAL
             if floor==1:
-                recent_fun_article = max(dc_api.iterableBoard(board=board, is_miner=is_miner, num=50, sess=sess), key=lambda doc: doc["views"] + doc["votes"]*10)
-                scene = scene.format_map(SafeDict(recent_fun_article=("%s [%d]" % (recent_fun_article["title"], recent_fun_article["comments"])), name=last_answerer))
+                recent_fun_article = max(dc_api.board(board_id=board, num=50, sess=sess), key=lambda doc: doc["view_num"] + doc["voteup_num"]*10)
+                scene = scene.format_map(SafeDict(recent_fun_article=("%s [%d]" % (recent_fun_article["title"], recent_fun_article["comment_num"])), name=last_answerer))
                 title_format = TITLE_FORMAT_ATTRACT
+            elif floor==-1:
+                for i in range(10):
+                    commenter.append("누군가")
+                answerers += ["베리나", "국주", "김제동", "박근혜"]
+                scene = scene.format_map(SafeDict([("commenter%d"%i, com) for i, com in enumerate(commenters)] + [("answerer%d"%i, ans) for i, ans in enumerate(answerer)]))
+                title_format = TITLE_FORMAT_COLLAPSED
             else:
                 scene = scene.format_map(SafeDict(name=last_answerer))
             scene = korean.l10n.proofread(scene)
             if floor==1:
-                doc_no = dc_api.writeDoc(board=board, is_miner=is_miner, name=NAME, password=NAME, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=last_answerer))), contents=scene, sess=sess)
-                dc_api.writeComment(board=board, is_miner=is_miner, doc_no=doc_no, name=NAME, password=PASSWORD, contents="-- 현제 층: 지하 1층 --", sess=sess)
+                doc_id = dc_api.write_document(board_id=board, name=NAME, pw=PASSWORD, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=last_answerer))), contents=scene, sess=sess)
+                time.sleep(2)
+                dc_api.write_comment(board_id=board, doc_id=doc_id, name=NAME, pw=PASSWORD, contents="-- 현제 층: 지하 1층 --", sess=sess)
+            elif floor==-1:
+                doc_id = dc_api.write_document(board_id=board, name=NAME, pw=PASSWORD, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=last_answerer))), contents=scene, sess=sess)
+                time.sleep(BREAK_TIME)
+                return
             else:
-                doc_no = dc_api.modifyDoc(board=board, is_miner=is_miner, doc_no=doc_no, name=NAME, password=PASSWORD, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=last_answerer))), contents=scene, sess=sess)
+                doc_id = dc_api.write_document(board_id=board, name=NAME, pw=PASSWORD, title=korean.l10n.proofread(title_format.format_map(SafeDict(floor=floor, name=last_answerer))), contents=scene, sess=sess)
+                #time.sleep(2)
+                #dc_api.write_comment(board_id=board, doc_id=doc_id, name=NAME, pw=PASSWORD, contents=korean.l10n.proofread(ANSWER_COMMENT.format(name=last_answerer)), sess=sess)
+                time.sleep(2)
+                dc_api.write_comment(board_id=board, doc_id=doc_id, name=NAME, pw=PASSWORD, contents="-- 현제 층: 지하 %d층 -- / 문을 연사람: %s / 갇힌 사람: %s" % (floor, ", ".join(answerers), ", ".join(commenters)), sess=sess)
             solved = False
             last_answerer = ""
             while not solved:
-                for comment in dc_api.iterableComments(board=board, is_miner=is_miner, doc_no=doc_no, num=20, sess=sess):
-                    if compareAnswer(last_answers, comment["contents"]):
-                        dc_api.removeComment(board=board, is_miner=is_miner, doc_no=doc_no, comment_no=comment["comment_no"], password=PASSWORD, sess=sess)
-                        continue
+                if time.time() - start_time > COLLAPSED_THRESHOLD:
+                    dc_api.remove_document(board_id=board, doc_id=doc_id, pw=PASSWORD)
+                    floor = -1
+                    break
+                for comment in dc_api.comments(board_id=board, doc_id=doc_id, num=20, sess=sess):
+                    if comment["author"] not in commenters: commenters.append(comment["author"])
                     if compareAnswer(answers, comment["contents"]):
-                        last_answerer = comment["name"]
+                        dc_api.remove_document(board_id=board, doc_id=doc_id, pw=PASSWORD)
+                        last_answerer = comment["author"]
                         print(last_answerer)
                         if last_answerer not in answerers: answerers.append(last_answerer)
                         solved = True
                         floor += 1
-                        last_answers += answers
-                        for com in dc_api.iterableComments(board=board, is_miner=is_miner, doc_no=doc_no, num=20, sess=sess):
-                            dc_api.removeComment(board=board, is_miner=is_miner, doc_no=doc_no, comment_no=com["comment_no"], password=PASSWORD, sess=sess)
-                        dc_api.writeComment(board=board, is_miner=is_miner, doc_no=doc_no, name=NAME, password=PASSWORD, contents=korean.l10n.proofread(ANSWER_COMMENT.format(name=comment["name"])), sess=sess)
-                        dc_api.writeComment(board=board, is_miner=is_miner, doc_no=doc_no, name=NAME, password=PASSWORD, contents="-- 현제 층: 지하 %d층 -- / 갇힌 사람: %s" % (floor, ", ".join(answerers)), sess=sess)
                         print("floor: %d" % floor)
                         break
-                time.sleep(0.5)
+                time.sleep(5)
         except KeyboardInterrupt:
-            exit(1)
-            try: dc_api.removeDoc(board=board, is_miner=is_miner, doc_no=doc_no, password=PASSWORD, sess=sess)
+            try: dc_api.remove_document(board_id=board, doc_id=doc_id, pw=PASSWORD, sess=sess)
             except: pass
             exit(1)
         except Exception as e:
             print(e)
             traceback.print_exc()
+            '''
             print("repeat until success")
+            time.sleep(5)
             floor -= 1
             continue
+            '''
+            exit(1)
         
 
 if __name__ == '__main__':
-    run(BOARD, IS_MINER, ID, SECRET)
+    while True:
+        run(BOARD, ID, SECRET)
